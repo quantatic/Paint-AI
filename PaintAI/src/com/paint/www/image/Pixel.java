06ed6cecc4f7b670d31986d91f4c41e1fee62660
Itai F <chessandpotatoes@gmail.com>
2018-10-06 10:38:14 -0400

    itai sucks

--- PaintAI/src/com/paint/www/image/Pixel.java
+++ PaintAI/src/com/paint/www/image/Pixel.java
@@ -3,51 +3,54 @@ package com.paint.www.image;
 public class Pixel {
 	private int colorValues;
 	
-	public Pixel(char red, char green, char blue, char alpha) {
+	public Pixel() {
+		this(0, 0, 0, 0);
+	}
+	public Pixel(int red, int green, int blue, int alpha) {
 		setRed(red);
 		setGreen(green);
 		setBlue(blue);
 		setAlpha(alpha);
 	}
 
-	public void setAlpha(char alpha) {
+	public void setAlpha(int alpha) {
 		if(alpha < 0 || alpha > 255) {
 			throw new IllegalArgumentException("Illegal Alpha Value");
 		}
 		colorValues = (colorValues & 0xFF_FF_FF_00) | (alpha);
 	}
 
-	public void setBlue(char blue) {
+	public void setBlue(int blue) {
 		if(blue < 0 || blue > 255) {
 			throw new IllegalArgumentException("Illegal Blue Value");
 		}
 		colorValues = (colorValues & 0xFF_FF_00_FF) | (blue << 8);
 	}
 
-	public void setGreen(char green) {
+	public void setGreen(int green) {
 		if(green < 0 || green > 255) {
 			throw new IllegalArgumentException("Illegal Green Value");
 		}
 		colorValues = (colorValues & 0xFF_00_FF_FF) | (green << 16);
 	}
 
-	public void setRed(char red) {
+	public void setRed(int red) {
 		if(red < 0 || red > 255) {
 			throw new IllegalArgumentException("Illegal Red Value");
 		}
 		colorValues = (colorValues & 0x00_FF_FF_FF) | (red << 24);
 	}
 	
-	public char getRed() {
-		return  (char) ((colorValues >> 24) & 0xFF);
+	public int getRed() {
+		return   ((colorValues >> 24) & 0xFF);
 	}
-	public char getGreen() {
-		return (char) ((colorValues >> 16) & 0xFF);
+	public int getGreen() {
+		return  ((colorValues >> 16) & 0xFF);
 	}
-	public char getBlue() {
-		return (char) ((colorValues >> 8) & 0xFF);
+	public int getBlue() {
+		return  ((colorValues >> 8) & 0xFF);
 	}
-	public char getAlpha() {
-		return (char) (colorValues & 0xFF);
+	public int getAlpha() {
+		return  (colorValues & 0xFF);
 	}
 }

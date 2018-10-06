5f5cfa492f1209869a538bd09419d48610201f86
Itai F <chessandpotatoes@gmail.com>
2018-10-06 10:01:29 -0400

    more shtuff

--- PaintAI/src/com/paint/www/image/Pixel.java
+++ PaintAI/src/com/paint/www/image/Pixel.java
@@ -1,5 +1,11 @@
 package com.paint.www.image;
 
 public interface Pixel {
-
+	
+	
+	public double getOpacity();
+	public char getRed();
+	public char getGreen();
+	public char getBlue();
+	public Pixel blend();
 }
